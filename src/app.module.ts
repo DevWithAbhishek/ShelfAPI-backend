a002af3c61414f1e4dbb4cebcf0e5519afc8d5b3
Abhishek91K <abhishek@codewithabhishek.in>
2026-08-24 20:46:25 +0530

    fix: remove duplicate docs service registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,17 @@ import { UsersModule } from './users/users.module';
 import { DocsModule } from './docs/docs.module';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { AuthService } from './auth/auth.service';
-import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
-import { DocsService } from './docs/docs.service';
-import { DocsController } from './docs/docs.controller';
 
 @Module({
-  imports: [UsersModule, DocsModule, HealthModule, AuthModule,ConfigModule.forRoot({isGlobal: true}), DocsModule],
-  controllers: [AppController, AuthController, UsersController, DocsController],
-  providers: [AppService, UsersService, AuthService, PrismaService, JwtService, DocsService],
+  imports: [
+    UsersModule,
+    DocsModule,
+    HealthModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
