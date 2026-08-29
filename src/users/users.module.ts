25d7afa1534737e22252cefe6fd8c3fbc20e097a
Abhishek91K <abhishek@codewithabhishek.in>
2026-08-30 01:30:49 +0530

    fix: remove circular AuthModule<->UsersModule dependency, import JwtModule directly

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
+  exports: [UsersService],
 })
 export class UsersModule {}
